eb53fb1b913b31fe1b5e3a56d2b71f8c3b0de7a4
sfshariff <sana.f.sharifff@gmail.com>
2014-11-27 12:57:13 -0500

    Main added - not complete

---
+++ Assignment3_Sana/src/Provided.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Provided {
+	public ArrayList<String > capability;
+	public ArrayList<String> binary;
+	
+	public Provided()
+	{
+		capability = new ArrayList<String>();
+		binary = new ArrayList<String>();
+	}
+}
